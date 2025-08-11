4d877c3bf2b3394bee4257658131ef0d84244746
rishabh-jain26 <4326rishabhjain@gmail.com>
2025-08-11 12:10:25 +0530

    find area of circle

---
+++ javabasics/AreaOfCircle.java
@@ -0,0 +1,10 @@
+package BridgeLabz_PPP.javabasics;
+import java.util.Scanner;
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int radius = sc.nextInt();
+        double area = 3.14 * radius * radius;
+        System.out.print(area);
+    }
+}
