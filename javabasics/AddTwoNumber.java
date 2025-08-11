d8aef713fd05a604a2ef857d92f41c65a77aecc4
rishabh-jain26 <4326rishabhjain@gmail.com>
2025-08-11 11:58:27 +0530

    add two numbers

---
+++ javabasics/AddTwoNumber.java
@@ -0,0 +1,11 @@
+package BridgeLabz_PPP.javabasics;
+import java.util.Scanner;
+public class AddTwoNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int add = n1+n2;
+        System.out.print(add);
+    }
+}
